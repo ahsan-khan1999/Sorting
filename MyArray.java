2398ae452b318bdf100476910e49a58968c312c8
Ahsan Khan <36322910+ahsan-khan1999@users.noreply.github.com>
2018-10-17 03:06:41 -0700

    Add files via upload

---
+++ MyArray.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class MyArray{
+
+
+    // generic method
+//    public static <E extends Comparable> void inspectArray(E[] inputArray) {
+//        for (int i = 1; i < inputArray.length; i++) {
+//            if(inputArray[i].compareTo(inputArray[i+1]) >=0){
+//                E temp = inputArray[i];
+//                inputArray[i++] = inputArray[i];
+//                temp =
+////            int tmp = inputArray[i - 1].compareTo(inputArray[i]);
+            //unreated lines are neglected
+        }
+    }
+//        MyArray<Integer> myArray = new MyArray<Integer>();
+//    //    List<T> list = new ArrayList<T>();
+//    public <T> void sort(MyArray<T> myArray){
+//
+//        for(int i = 0 ; i < 10 ; i++){
+//            if(myArray[i].compareTo(myArray[i++]))
+//        }
+//    }
+//    public void compareTo
+
+
+        //    int unSortArray[] = {2,1,4,5,2,34};
+////    {1,2,3,4,7,5}
+//    for(int i = 1 ; i < unSortArray.length ; i++){
+//        i=i-1;
+//        if(unSortArray[i] < unSortArray[i++]){
+//            System.out.println("Clear"+unSortArray[i]);
+//        }
+//        else{
+//            int temp=0;
+//            unSortArray[i] = temp;
+//            unSortArray[i] = unSortArray[i+1];
+//            unSortArray[i+1] = temp;
+//            System.out.println(unSortArray[i]);
+//            i--;
+//        }
+//
+//    }
+
+//    for (int i = 0 ; i < unSortArray.length ; i++){
+//        System.out.println(unSortArray[i]+"holllll");
+//    }
+
+//    public static <T extends Comparable<T>> int countGreaterThan(T[] anArray, T elem) {
+//        int count = 0;
+//        for (T e : anArray) {
+//            if (e.compareTo(elem) > 0) {
+//
+//                ++count;
+//            }
+//        }
+//        return count;
+//    }
+
+//                temp = list[j];
+//                list[j] = list[j+1];
+//                list[j+1] = temp;
+//                j--;
+
+        }}
+
+
+
+
---
+++ TestSorting.java
@@ -0,0 +1,19 @@
+public class TestSorting {
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray();
+
+//        Integer[] array  ={2,10,5,87,3,5};
+//        MyArray.inspectArray(array);
+//        for (int i = 0 ; i < array.length ; i++){
+//            System.out.println(array[i]);
+//        }
+//        MyArray myArray1 = new MyArray();
+//        MyArray.countGreaterThan(myArray,myArray1);
+//        int newArray[]={2,1,3,5,7,4,0};
+//        myArray.insertionSorting(newArray);
+//        for (int i = 0; i < newArray.length; i++) {
+//            System.out.println(newArray[i]);
+//
+//        }
+    }
+}
